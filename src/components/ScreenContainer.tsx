859870d339127fef64e8d67c691188491b6d4b13
jbarget <dear.justen@gmail.com>
2021-01-18 14:02:17 +0000

    adds Genres screen and transitions away from Home

---
+++ src/components/ScreenContainer.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+const ScreenContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+`;
+
+export default ScreenContainer;
